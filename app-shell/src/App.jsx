1f66c85bba2a84978f67acd9b15b0184d1172291
paras-1999 <paras.saxena@neosoftmail.com>
2022-05-10 16:30:32 +0530

    first commit

---
+++ app-shell/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Login from "Login/Login"
+// import "./index.css";
+
+const App = () => (
+  <>
+   <Login/>
+  </>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
---
+++ login/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Login from "./Login";
+
+const App = () => (
+  <>
+  <Login/>
+  </>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
