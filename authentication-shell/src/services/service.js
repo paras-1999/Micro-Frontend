acebdd8ac74f8391bd32ceea8cde0d9d28b4c229
rohit <rohit.gupta@neosoftmail.com>
2022-05-16 14:52:42 +0530

    refactored the code to use api-factory

--- authentication-shell/src/services/service.js
+++ authentication-shell/src/services/service.js
@@ -1,23 +1,25 @@
-import axios from "axios"
+import apiFactory from "../utils/api";
 import { BehaviorSubject } from "rxjs";
-const API_SERVER = "http://localhost:8080";
+// const API_SERVER = "http://localhost:8080";
 export const jwt = new BehaviorSubject(null);
-export const loginCall = (username, password) =>
-    axios.post(`${API_SERVER}/auth/login`,
-        {
-            username: username,
-            password: password,
-        }
 
-    )
-        .then((res) => {
-            console.log(res.data.access_token)
-            jwt.next(res.data.access_token);
-            return res.data.access_token;
-        });
+export const loginCall = async (username, password) => {
+    const data = {
+        username,
+        password
+    }
+
+    const res = await apiFactory().post("/auth/login", data);
+    
+    console.log(res.data.access_token)
+    jwt.next(res.data.access_token);
+    return res.data.access_token;
+}
+
 
 export async function getProfile(token) {
-    return await axios.get(`${API_SERVER}/authorized`, {
-        headers: { "authorization": `Bearer ${token}` }
-    });
+    return apiFactory().get("/authorized");
+    // return await axios.get(`${API_SERVER}/authorized`, {
+    //     headers: { "authorization": `Bearer ${token}` }
+    // });
 }
